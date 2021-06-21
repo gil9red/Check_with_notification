f7bf277768191c0ef51dc9a8e7c612787255ff3a
ipetrash <ilya.petrash@inbox.ru>
2021-06-21 18:55:50 +0500

    Added "Check new video Рик и Морти"

---
+++ Check new video Рик и Морти/main.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+"""
+Скрипт для уведомления о появлении новых видео Рик и Морти.
+
+"""
+
+
+import sys
+
+from pathlib import Path
+from typing import List
+
+DIR = Path(__file__).resolve().parent
+sys.path.append(str(DIR.parent))  # Путь к папке выше
+
+from root_common import run_notification_job, FORMAT_VIDEO
+from third_party.rik_i_morti_ru import get_season_by_series
+
+
+def get_video_list() -> List[str]:
+    items = []
+    for video_list in get_season_by_series().values():
+        items += video_list
+
+    return items
+
+
+run_notification_job(
+    'Рик и Морти',
+    DIR,
+    get_video_list,
+    timeout={'days': 1},
+    format=FORMAT_VIDEO,
+)
