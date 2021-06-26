61e1ed4ef1cf375e42bad651dd15844e64eca6c5
ipetrash <ilya.petrash@inbox.ru>
2021-06-26 23:15:11 +0500

    Updated. Using 'run_notification_job'

--- Check for new chapters of the manga/main.py
+++ Check for new chapters of the manga/main.py
@@ -8,19 +8,14 @@ import sys
 from pathlib import Path
 from typing import List
 
-DIR = Path(__file__).resolve().parent
-sys.path.append(str(DIR.parent))  # Путь к папке выше
-
 import feedparser
 import requests
 
-from root_common import get_logger, send_telegram_notification, wait
+DIR = Path(__file__).resolve().parent
+sys.path.append(str(DIR.parent))  # Путь к папке выше
 
-
-# TODO: попробовать реализовать через run_notification_job
-
-
-log = get_logger('Новые главы манги')
+from format import FORMAT_CHAPTER
+from root_common import run_notification_job
 
 
 def get_feeds_by_manga_chapters(url_rss: str) -> List[str]:
@@ -45,75 +40,10 @@ def get_feeds_by_manga_chapters(url_rss: str) -> List[str]:
 
 URL_USER_RSS = 'https://grouple.co/user/rss/315828?filter='
 
-DIR = Path(__file__).resolve().parent
-FILE_NAME_LAST_FEED = DIR / 'last_feed'
-FILE_NAME_SKIP = DIR / 'skip'
 
-
-def save_last_feed(feed):
-    open(FILE_NAME_LAST_FEED, 'w', encoding='utf-8').write(feed)
-
-
-if __name__ == '__main__':
-    need_notification = True
-
-    # Загрузка последней новости
-    try:
-        last_feed = open(FILE_NAME_LAST_FEED, encoding='utf-8').read()
-    except:
-        last_feed = ""
-
-    while True:
-        if FILE_NAME_SKIP.exists():
-            log.info('Обнаружен файл "%s", пропускаю проверку.', FILE_NAME_SKIP.name)
-            wait(days=7)
-            continue
-
-        try:
-            log.debug('get_feeds_by_manga_chapters')
-            log.debug('Last feed: "%s"', last_feed)
-
-            current_feeds = get_feeds_by_manga_chapters(URL_USER_RSS)
-            log.debug('current_feeds: %s', current_feeds)
-
-            if not last_feed or last_feed not in current_feeds:
-                # Считаем что это первый запуск
-                last_feed = current_feeds[0]
-                log.debug('Первый запуск, запоминаю последнюю главу: "{}"'.format(last_feed))
-
-                save_last_feed(last_feed)
-
-            # Если последняя новость есть в списке текущих новостей
-            else:
-                index = current_feeds.index(last_feed)
-
-                # Получаем список новостей после последней новости
-                new_feeds = current_feeds[:index]
-                if new_feeds:
-                    last_feed = new_feeds[0]
-                    save_last_feed(last_feed)
-
-                    log.debug('Вышло:')
-                    for manga in new_feeds:
-                        log.debug('    ' + manga)
-
-                    if need_notification:
-                        text = 'Новые главы: {}'.format(len(new_feeds))
-                        send_telegram_notification(log.name, text)
-
-                else:
-                    log.debug('Новых глав нет')
-
-            wait(days=7)
-
-        except requests.exceptions.ConnectionError as e:
-            log.warning('Ошибка подключения к сети: %s', e)
-            log.debug('Через минуту попробую снова...')
-
-            wait(minutes=1)
-
-        except:
-            log.exception('Непредвиденная ошибка:')
-            log.debug('Через 5 минут попробую снова...')
-
-            wait(minutes=5)
+run_notification_job(
+    'Новые главы манги',
+    DIR,
+    lambda: get_feeds_by_manga_chapters(URL_USER_RSS),
+    format=FORMAT_CHAPTER,
+)
