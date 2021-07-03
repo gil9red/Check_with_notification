1540faba5b2a980292802d277f0285590b3f83c3
ipetrash <ilya.petrash@inbox.ru>
2021-07-03 14:54:42 +0500

    Added "Check new video Metroid Series Story"

---
+++ Check new video Metroid Series Story/main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+"""
+Скрипт для уведомления о появлении новых видео истории серии Metroid.
+
+"""
+
+
+import sys
+from pathlib import Path
+
+DIR = Path(__file__).resolve().parent
+sys.path.append(str(DIR.parent))  # Путь к папке выше
+
+from format import FORMAT_VIDEO
+from root_common import run_notification_job, get_playlist_video_list
+
+
+run_notification_job(
+    'История серии Metroid [StopGame]',
+    DIR,
+    lambda: get_playlist_video_list('PLZfhqd1-Hl3Di2yxyrrvH53vdREDrHU3c'),
+    format=FORMAT_VIDEO,
+)
