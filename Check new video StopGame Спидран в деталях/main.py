04f22a3b25d2fdc5ac06cfed3d3a346c47bf6e11
ipetrash <ilya.petrash@inbox.ru>
2022-02-07 22:13:57 +0500

    Added "Check new video StopGame Спидран в деталях"

---
+++ Check new video StopGame Спидран в деталях/main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+"""
+Скрипт для уведомления о появлении новых видео "Спидран в деталях".
+
+"""
+
+
+import sys
+from pathlib import Path
+
+DIR = Path(__file__).resolve().parent
+sys.path.append(str(DIR.parent))  # Путь к папке выше
+
+from formats import FORMATS_VIDEO
+from root_common import run_notification_job, get_playlist_video_list
+
+
+run_notification_job(
+    'Спидран в деталях [StopGame]',
+    DIR,
+    lambda job: get_playlist_video_list('PLZfhqd1-Hl3D5Y_DW3fD9lyaclvt89XsI'),
+    formats=FORMATS_VIDEO,
+)
