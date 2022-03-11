e5b1713cc6bc7c8e2862681dae20a6a4fc9c33a0
ipetrash <ilya.petrash@inbox.ru>
2022-03-12 03:17:48 +0500

    Updated. Update to new version. Supporting url in telegram notifications

--- Check new video Рик и Морти/main.py
+++ Check new video Рик и Морти/main.py
@@ -20,7 +20,7 @@ sys.path.append(str(DIR.parent))  # Путь к папке выше
 
 from formats import FORMATS_VIDEO
 from root_common import run_notification_job, NotificationJob
-from third_party.rik_i_morti_online import get_season_by_series
+from third_party.rik_i_morti_online import URL, get_season_by_series
 
 
 def get_video_list(job: NotificationJob) -> List[str]:
@@ -36,4 +36,5 @@ run_notification_job(
     DIR,
     get_video_list,
     formats=FORMATS_VIDEO,
+    url=URL,
 )
