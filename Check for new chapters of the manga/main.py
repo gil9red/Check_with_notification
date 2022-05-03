52b6d94d4c9bd567411c47039ba0cf4bb5839b2f
gil9red <ilya.petrash@inbox.ru>
2022-05-03 23:34:53 +0500

    Refactoring

--- Check for new chapters of the manga/main.py
+++ Check for new chapters of the manga/main.py
@@ -6,10 +6,8 @@ __author__ = 'ipetrash'
 
 import sys
 from pathlib import Path
-from typing import List
 
 import feedparser
-import requests
 
 DIR = Path(__file__).resolve().parent
 sys.path.append(str(DIR.parent))  # Путь к папке выше
@@ -18,13 +16,14 @@ from formats import FORMATS_CHAPTER
 from root_common import run_notification_job, TimeoutWait
 
 
-def get_feeds_by_manga_chapters(url_rss: str) -> List[str]:
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0',
-    }
+URL_USER_RSS = 'https://grouple.co/user/rss/315828?filter='
 
-    rss_text = requests.get(url_rss, headers=headers).text
-    feed = feedparser.parse(rss_text)
+
+def get_feeds_by_manga_chapters() -> list[str]:
+    feed = feedparser.parse(
+        URL_USER_RSS,
+        agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0',
+    )
 
     feeds = []
     for entry in feed.entries:
@@ -40,13 +39,10 @@ def get_feeds_by_manga_chapters(url_rss: str) -> List[str]:
     return feeds
 
 
-URL_USER_RSS = 'https://grouple.co/user/rss/315828?filter='
-
-
 run_notification_job(
     'Новые главы манги',
     DIR,
-    lambda job: get_feeds_by_manga_chapters(URL_USER_RSS),
+    lambda job: get_feeds_by_manga_chapters(),
     timeout=TimeoutWait(hours=8),
     formats=FORMATS_CHAPTER,
     need_to_store_items=500,  # Будем помнить последние 500 глав
