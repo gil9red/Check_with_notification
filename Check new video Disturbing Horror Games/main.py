f01f77dbceb37550ad23221e8bb9d139805ee26f
gil9red <ilya.petrash@inbox.ru>
2022-07-30 00:09:58 +0500

    Updated. Added filter

--- Check new video Disturbing Horror Games/main.py
+++ Check new video Disturbing Horror Games/main.py
@@ -17,12 +17,22 @@ DIR = Path(__file__).resolve().parent
 sys.path.append(str(DIR.parent))  # Путь к папке выше
 
 from formats import FORMATS_VIDEO
-from root_common import run_notification_job, get_playlist_video_list
+from root_common import run_notification_job, get_playlist_video_list, DataItem, NotificationJob
+
+
+# NOTE: Нужна фильтрация, чтобы оставить только нужные видео
+#       Т.к. в плейлисте иногда попадают видео типа "Klasky Csupo in Pixitracker Major 9312"
+def get_items(_: NotificationJob) -> list[DataItem]:
+    return [
+        item
+        for item in get_playlist_video_list('PLVOZT4ssBLx7d4TSZuYU_lkRwAUaON3FI')
+        if 'DHG #' in item.title
+    ]
 
 
 run_notification_job(
     'Disturbing Horror Games',
     DIR,
-    lambda job: get_playlist_video_list('PLVOZT4ssBLx7d4TSZuYU_lkRwAUaON3FI'),
+    get_items,
     formats=FORMATS_VIDEO,
 )
