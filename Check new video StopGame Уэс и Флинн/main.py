53ae360018727151d83eca9f587667de4319d34c
gil9red <ilya.petrash@inbox.ru>
2022-10-25 19:05:30 +0500

    Added "Check new video StopGame Уэс и Флинн"

---
+++ Check new video StopGame Уэс и Флинн/main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+"""
+Скрипт для уведомления о появлении новых видео Уэс и Флинн.
+
+"""
+
+
+import sys
+from pathlib import Path
+
+DIR = Path(__file__).resolve().parent
+sys.path.append(str(DIR.parent))  # Путь к папке выше
+
+from formats import FORMATS_VIDEO
+from root_common import run_notification_job, get_playlist_video_list
+
+
+run_notification_job(
+    'Уэс и Флинн [StopGame]',
+    DIR,
+    lambda job: get_playlist_video_list('PLZfhqd1-Hl3C5AQ6LPcMsVBIpduqckjPB'),
+    send_new_items_separately=True,
+    formats=FORMATS_VIDEO,
+)
