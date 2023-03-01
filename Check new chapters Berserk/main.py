c18b1644099047c950019d2b131bd5e8d854e30a
gil9red <ilya.petrash@inbox.ru>
2023-03-01 22:35:51 +0500

    Bugfix. "Check new chapters Berserk"

--- Check new chapters Berserk/main.py
+++ Check new chapters Berserk/main.py
@@ -17,14 +17,24 @@ DIR = Path(__file__).resolve().parent
 sys.path.append(str(DIR.parent))  # Путь к папке выше
 
 from formats import FORMATS_CHAPTER
-from root_common import run_notification_job
-from third_party.risens_team__berserk import get_chapters, URL
+from root_common import DataItem, run_notification_job, NotificationJob
+from third_party.gitmanga_com__get_chapters import get_chapters
+
+
+def get_items(_: NotificationJob) -> list[DataItem]:
+    url = 'https://gitmanga.com/764-berserk.html'
+    return [
+        DataItem(
+            value=chapter.title,
+            url=chapter.url,
+        )
+        for chapter in get_chapters(url)
+    ]
 
 
 run_notification_job(
     'Манга Berserk',
     DIR,
-    lambda job: get_chapters(),
+    get_items,
     formats=FORMATS_CHAPTER,
-    url=URL,
 )
