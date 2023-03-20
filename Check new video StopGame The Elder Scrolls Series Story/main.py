ee38e5d2877805af57611c9943c107a1fdbd699d
gil9red <ilya.petrash@inbox.ru>
2023-03-20 16:56:12 +0500

    Added "Check new video StopGame The Elder Scrolls Series Story"

---
+++ Check new video StopGame The Elder Scrolls Series Story/main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+"""
+Скрипт для уведомления о появлении новых видео истории серии The Elder Scrolls.
+
+"""
+
+
+import sys
+from pathlib import Path
+
+DIR = Path(__file__).resolve().parent
+sys.path.append(str(DIR.parent))  # Путь к папке выше
+
+from formats import FORMATS_VIDEO
+from root_common import run_notification_job, get_playlist_video_list
+
+
+run_notification_job(
+    'История серии The Elder Scrolls [StopGame]',
+    DIR,
+    lambda job: get_playlist_video_list('PLZfhqd1-Hl3C0B3dmwhuKKzUJ-H30gGVj'),
+    send_new_items_separately=True,
+    formats=FORMATS_VIDEO,
+)
