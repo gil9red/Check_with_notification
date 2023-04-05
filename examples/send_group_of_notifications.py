d7761475f2ae8a0fb898aeecb7f834fc7239fb8a
gil9red <ilya.petrash@inbox.ru>
2023-04-06 01:07:09 +0500

    Added "examples/send_group_of_notifications.py"

---
+++ examples/send_group_of_notifications.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+
+"""
+Скрипт для примера отправки группы уведомлений.
+
+"""
+
+
+import sys
+import uuid
+
+from pathlib import Path
+
+DIR = Path(__file__).resolve().parent
+sys.path.append(str(DIR.parent))  # Путь к папке выше
+
+from root_common import NotificationJob, run_notification_job
+
+
+def get_items(_: NotificationJob) -> list[str]:
+    return [
+        f'#{i + 1}. {uuid.uuid4()}'
+        for i in range(5)
+    ]
+
+
+run_notification_job(
+    'Тест',
+    DIR,
+    get_items,
+    send_new_items_as_group=True
+)
