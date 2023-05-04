640ee0edc7b1baafe88b6c72b337c369c27122dd
gil9red <ilya.petrash@inbox.ru>
2023-05-05 02:23:06 +0500

    Added "Check new video StopGame Хардмод"

---
+++ Check new video StopGame Хардмод/main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "ipetrash"
+
+
+"""
+Скрипт для уведомления о появлении новых видео Хардмод.
+
+"""
+
+
+import sys
+from pathlib import Path
+
+DIR = Path(__file__).resolve().parent
+sys.path.append(str(DIR.parent))  # Путь к папке выше
+
+from formats import FORMATS_VIDEO
+from root_common import run_notification_job, get_playlist_video_list
+
+
+run_notification_job(
+    "Хардмод [StopGame]",
+    DIR,
+    lambda _: get_playlist_video_list("PLZfhqd1-Hl3BCuapQSaNrNyDAy3wLiOLL"),
+    send_new_items_separately=True,
+    formats=FORMATS_VIDEO,
+)
