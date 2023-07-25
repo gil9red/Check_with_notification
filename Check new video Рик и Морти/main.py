badd96643559600b6fdfe08bdf6f5ee5468dcd4f
gil9red <ilya.petrash@inbox.ru>
2023-07-25 22:52:09 +0500

    Bugfix. "Check new video Рик и Морти"

--- Check new video Рик и Морти/main.py
+++ Check new video Рик и Морти/main.py
@@ -18,22 +18,14 @@ DIR = Path(__file__).resolve().parent
 sys.path.append(str(DIR.parent))  # Путь к папке выше
 
 from formats import FORMATS_VIDEO
-from root_common import run_notification_job, NotificationJob
-from third_party.rik_i_morti_online import URL, get_season_by_series
-
-
-def get_video_list(_: NotificationJob) -> list[str]:
-    items = []
-    for video_list in get_season_by_series().values():
-        items += video_list
-
-    return items
+from root_common import run_notification_job
+from third_party.ru_wikipedia_org__wiki__Список_эпизодов_мультсериала_Рик_и_Морти import get_all_series
 
 
 run_notification_job(
     "Рик и Морти",
     DIR,
-    get_video_list,
+    lambda job: get_all_series(),
     formats=FORMATS_VIDEO,
-    url=URL,
+    url="https://rick-i-morty.online/",
 )
