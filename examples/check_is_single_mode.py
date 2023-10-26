4b1f061fa76ff41d756e1109b063999d2be92024
gil9red <ilya.petrash@inbox.ru>
2023-10-26 19:19:54 +0500

    Refactoring

--- examples/check_is_single_mode.py
+++ examples/check_is_single_mode.py
@@ -16,9 +16,22 @@ from pathlib import Path
 DIR = Path(__file__).resolve().parent
 sys.path.append(str(DIR.parent))  # Путь к папке выше
 
+# Use argument
+sys.argv.append("--single")
+
 from root_common import run_notification_job
 
 
+run_notification_job(
+    "Тест",
+    DIR,
+    lambda _: ["1", "2", "3"],
+    need_notification=False,
+)
+
+print("\n" + "-" * 50 + "\n")
+
+sys.argv.remove("--single")
 run_notification_job(
     "Тест",
     DIR,
@@ -27,12 +40,14 @@ run_notification_job(
     need_notification=False,
 )
 
-# NOTE: process error
-# run_notification_job(
-#     "Тест",
-#     DIR,
-#     lambda _: [str(1/0)],
-#     is_single=True,
-#     max_attempts_for_is_single=1,
-#     need_notification=False,
-# )
+print("\n" + "-" * 50 + "\n")
+
+# Process error
+run_notification_job(
+    "Тест",
+    DIR,
+    lambda _: [str(1/0)],
+    is_single=True,
+    max_attempts_for_is_single=1,
+    need_notification=False,
+)
