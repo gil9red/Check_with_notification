a5469ef624c6bd436fa2197e4104684ee2be8c31
gil9red <ilya.petrash@inbox.ru>
2023-12-10 21:06:58 +0500

    Added "Check new video Кубрик"

---
+++ Check new video Кубрик/main.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "ipetrash"
+
+
+"""
+Скрипт для уведомления о появлении новых видео Кубрик.
+
+"""
+
+
+import sys
+from pathlib import Path
+
+DIR = Path(__file__).resolve().parent
+sys.path.append(str(DIR.parent))  # Путь к папке выше
+
+from formats import FORMATS_VIDEO
+from root_common import (
+    SavedModeEnum,
+    get_yt_video_list,
+    run_notification_job,
+)
+
+
+run_notification_job(
+    "Кубрик",
+    DIR,
+    lambda _: get_yt_video_list("https://www.youtube.com/@kubrik1985/videos"),
+    save_mode=SavedModeEnum.DATA_ITEM,
+    send_new_items_separately=True,
+    formats=FORMATS_VIDEO.replace(
+        prefix="🧊",
+    ),
+)
