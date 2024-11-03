30f3158145879226896c54d268c301eb46d78e1f
gil9red <ilya.petrash@inbox.ru>
2024-11-04 00:50:00 +0500

    Added "Check new video ХтоШо"

---
+++ Check new video ХтоШо/main.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "ipetrash"
+
+
+"""
+Скрипт для уведомления о появлении новых видео ХтоШо.
+
+"""
+
+
+import sys
+from pathlib import Path
+
+DIR = Path(__file__).resolve().parent
+sys.path.append(str(DIR.parent))  # Путь к папке выше
+
+from formats import FORMATS_VIDEO
+from root_common import (
+    run_notification_job,
+    SavedModeEnum,
+    get_yt_video_list,
+)
+
+
+run_notification_job(
+    "ХтоШо",
+    DIR,
+    lambda _: get_yt_video_list("https://www.youtube.com/@HtoShoChannel/videos"),
+    save_mode=SavedModeEnum.DATA_ITEM,
+    send_new_items_separately=True,
+    formats=FORMATS_VIDEO.replace(
+        prefix="⚡",
+    ),
+)
