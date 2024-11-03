2b764161bc6ca36c12d5e847ef71d6e574b650a2
gil9red <ilya.petrash@inbox.ru>
2024-11-04 00:50:23 +0500

    Added "Check new video rutube - Мама в шапке"

---
+++ Check new video rutube - Мама в шапке/main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "ipetrash"
+
+
+"""
+Скрипт для уведомления о появлении новых видео "Мама в шапке".
+
+"""
+
+
+from pathlib import Path
+from root_common import run_notification_job_rutube
+from formats import FORMATS_VIDEO
+
+
+run_notification_job_rutube(
+    name="Мама в шапке",
+    script_dir=Path(__file__).resolve().parent,
+    url="https://rutube.ru/u/mamavshapke/videos/",
+    formats=FORMATS_VIDEO.replace(
+        prefix="🧙‍♀️",
+    ),
+)
