8c53ed6e4f55a50ec7e60c81971aad2b0a02a899
gil9red <ilya.petrash@inbox.ru>
2025-01-15 18:26:36 +0500

    Added "Check new video Александр N и YouHollywood"

---
+++ Check new video Александр N и YouHollywood/main.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "ipetrash"
+
+
+"""
+Скрипт для уведомления о появлении новых видео Александр N и YouHollywood.
+
+"""
+
+
+import sys
+from pathlib import Path
+
+DIR = Path(__file__).resolve().parent
+sys.path.append(str(DIR.parent))  # Путь к папке выше
+
+from formats import FORMATS_VIDEO
+from root_common import (
+    run_notification_job,
+    SavedModeEnum,
+    get_yt_video_list,
+    DataItem,
+    NotificationJob,
+)
+
+
+def get_items(_: NotificationJob) -> list[DataItem]:
+    # Не все видео интересны, убраны те, что связаны с курсами YouHollywood
+    url = "https://www.youtube.com/@YouHollywood/videos"
+    return [
+        item
+        for item in get_yt_video_list(url)
+        if (
+            "курс" not in item.title.lower()
+            and "youhollywood" not in item.title.lower()
+        )
+    ]
+
+
+run_notification_job(
+    "Александр N и YouHollywood",
+    DIR,
+    get_items,
+    save_mode=SavedModeEnum.DATA_ITEM,
+    send_new_items_separately=True,
+    formats=FORMATS_VIDEO,
+)
