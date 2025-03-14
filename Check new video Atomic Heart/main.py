ec38c5389db1b259576acd3cf4394c46c29d2c11
gil9red <ilya.petrash@inbox.ru>
2025-03-14 17:07:33 +0500

    Added "Check new video Atomic Heart"

---
+++ Check new video Atomic Heart/main.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "ipetrash"
+
+
+"""
+Скрипт для уведомления о появлении новых видео плейлиста Atomic Heart.
+
+"""
+
+
+import sys
+from pathlib import Path
+
+DIR = Path(__file__).resolve().parent
+sys.path.append(str(DIR.parent))  # Путь к папке выше
+
+from formats import FORMATS_VIDEO
+from root_common import (
+    SavedModeEnum,
+    get_yt_playlist_video_list,
+    run_notification_job,
+)
+
+
+run_notification_job(
+    "Atomic Heart",
+    DIR,
+    lambda _: get_yt_playlist_video_list("PLejGw9J2xE9UnwbubxPR2VsCjYo6VYuWr"),
+    save_mode=SavedModeEnum.DATA_ITEM,
+    send_new_items_separately=True,
+    formats=FORMATS_VIDEO,
+)
