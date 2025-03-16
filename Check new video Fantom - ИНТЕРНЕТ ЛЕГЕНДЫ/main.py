332785d1fde561ce3085f6460b21c1840cc31e38
gil9red <ilya.petrash@inbox.ru>
2025-03-16 13:33:13 +0500

    Added "Check new video Fantom - ИНТЕРНЕТ ЛЕГЕНДЫ"

---
+++ Check new video Fantom - ИНТЕРНЕТ ЛЕГЕНДЫ/main.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "ipetrash"
+
+
+"""
+Скрипт для уведомления о появлении новых видео плейлиста Fantom ИНТЕРНЕТ ЛЕГЕНДЫ.
+
+"""
+
+
+import sys
+from pathlib import Path
+
+DIR = Path(__file__).resolve().parent
+sys.path.append(str(DIR.parent))  # Путь к папке выше
+
+from formats import FORMATS_VIDEO
+from root_common import (
+    SavedModeEnum,
+    get_yt_playlist_video_list,
+    run_notification_job,
+)
+
+
+run_notification_job(
+    "ИНТЕРНЕТ ЛЕГЕНДЫ [Fantom]",
+    DIR,
+    lambda _: get_yt_playlist_video_list("PLqZA9icnp9qeUUh9ARJ7Wz3GyxEFCb9d4"),
+    save_mode=SavedModeEnum.DATA_ITEM,
+    send_new_items_separately=True,
+    formats=FORMATS_VIDEO,
+)
