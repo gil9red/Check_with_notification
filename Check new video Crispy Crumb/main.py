290c5dec8e346a1414f86c794681d154a08c3cc8
gil9red <ilya.petrash@inbox.ru>
2025-05-12 20:03:42 +0500

    Added "Check new video Crispy Crumb"

---
+++ Check new video Crispy Crumb/main.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "ipetrash"
+
+
+"""
+Скрипт для уведомления о появлении новых видео Crispy Crumb.
+
+"""
+
+
+import sys
+from pathlib import Path
+
+DIR = Path(__file__).resolve().parent
+sys.path.append(str(DIR.parent))  # Путь к папке выше
+
+from formats import FORMATS_VIDEO
+from root_common import (
+    run_notification_job,
+    SavedModeEnum,
+    get_yt_video_list,
+)
+
+
+run_notification_job(
+    "Crispy Crumb",
+    DIR,
+    lambda _: get_yt_video_list("https://www.youtube.com/@Crispy_Crumb/videos"),
+    save_mode=SavedModeEnum.DATA_ITEM,
+    formats=FORMATS_VIDEO.replace(
+        prefix="🍪",
+    ),
+)
