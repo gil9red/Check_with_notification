eec6cc43a76ca3dad01aef921ed4b3d9c1e25d0e
gil9red <ilya.petrash@inbox.ru>
2025-09-30 18:03:14 +0500

    Added. Check new video Кино Огонь - Полезность персонажей

---
+++ Check new video Кино Огонь - Полезность персонажей/main.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "ipetrash"
+
+
+"""
+Скрипт для уведомления о появлении новых видео Кино Огонь - Полезность персонажей
+
+"""
+
+
+import sys
+from pathlib import Path
+
+DIR = Path(__file__).resolve().parent
+sys.path.append(str(DIR.parent))  # Путь к папке выше
+
+from formats import FORMATS_VIDEO
+from root_common import (
+    run_notification_job,
+    SavedModeEnum,
+    get_yt_video_list,
+    DataItem,
+    NotificationJob,
+)
+
+
+def get_items(_: NotificationJob) -> list[DataItem]:
+    url = "https://www.youtube.com/@kinoogon/search?query=полезность полезный"
+    return [
+        item
+        for item in get_yt_video_list(url)
+        if "полезн" in item.title.lower()
+    ]
+
+
+run_notification_job(
+    "Кино Огонь",
+    DIR,
+    get_items,
+    save_mode=SavedModeEnum.DATA_ITEM,
+    formats=FORMATS_VIDEO,
+)
