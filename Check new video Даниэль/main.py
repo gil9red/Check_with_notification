183787099377586408b51053e50443911d0cf6b6
gil9red <ilya.petrash@inbox.ru>
2025-12-11 02:51:00 +0500

    Added. Check new video Даниэль

---
+++ Check new video Даниэль/main.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "ipetrash"
+
+
+"""
+Скрипт для уведомления о появлении новых видео Даниэль.
+
+"""
+
+
+import sys
+from pathlib import Path
+
+DIR = Path(__file__).resolve().parent
+sys.path.append(str(DIR.parent))  # Путь к папке выше
+
+from formats import FORMATS_VIDEO
+from root_common import (
+    run_notification_job,
+    SavedModeEnum,
+    get_yt_video_list,
+)
+
+
+run_notification_job(
+    "Даниэль",
+    DIR,
+    lambda _: get_yt_video_list("https://www.youtube.com/@Daniel-DungeonMasters/videos"),
+    save_mode=SavedModeEnum.DATA_ITEM,
+    formats=FORMATS_VIDEO.replace(
+        prefix="♂️",
+    ),
+)
