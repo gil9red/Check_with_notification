b3a126a50d6575bfbc62fe0e409edb49e1aaf592
gil9red <ilya.petrash@inbox.ru>
2026-03-31 10:51:03 +0500

    Added. Check new video Шизоидус

---
+++ Check new video Шизоидус/main.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "ipetrash"
+
+
+"""
+Скрипт для уведомления о появлении новых видео Шизоидус.
+
+"""
+
+
+import sys
+
+from pathlib import Path
+
+DIR = Path(__file__).resolve().parent
+sys.path.append(str(DIR.parent))  # Путь к папке выше
+
+from formats import FORMATS_VIDEO
+from root_common import (
+    run_notification_job,
+    SavedModeEnum,
+    get_yt_video_list,
+)
+
+
+run_notification_job(
+    "Шизоидус",
+    DIR,
+    lambda _: get_yt_video_list("https://www.youtube.com/@shizoidus/videos"),
+    save_mode=SavedModeEnum.DATA_ITEM,
+    formats=FORMATS_VIDEO.replace(
+        prefix="🍄",
+    ),
+)
