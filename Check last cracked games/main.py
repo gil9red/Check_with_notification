4e6e75adf0666eb60f15d8b782db79101ce0596d
gil9red <ilya.petrash@inbox.ru>
2026-04-01 15:58:42 +0500

    Updated. Check last cracked games

--- Check last cracked games/main.py
+++ Check last cracked games/main.py
@@ -10,7 +10,10 @@ __author__ = "ipetrash"
 """
 
 
+import html
 import sys
+
+from inspect import cleandoc
 from pathlib import Path
 
 DIR = Path(__file__).resolve().parent
@@ -24,18 +27,43 @@ from root_common import (
     SavedModeEnum,
     TimeoutWait,
 )
-from third_party.gamestatus_info__lastcrackedgames import get_games
+from third_party.gamestatus_info__lastcrackedgames import Game, get_games
+
+
+DATE_FORMAT: str = "%d.%m.%Y"
+
+
+def get_game_title(game: Game) -> str:
+    title: str = html.escape(game.title)
+    protection: str = html.escape(game.protection)
+    hacked: str = html.escape(game.hacked_groups)
+    release_date: str = (
+        game.release_date.strftime(DATE_FORMAT) if game.release_date else "Ожидается"
+    )
+    crack_date: str = (
+        game.crack_date.strftime(DATE_FORMAT) if game.crack_date else "Не взломана"
+    )
+
+    return cleandoc(
+        f"""
+        <b>{title}</b>
+        
+        📅 <b>Дата релиза:</b> <code>{release_date}</code>
+        🔓 <b>Дата взлома:</b> <code>{crack_date}</code>
+        🛡 <b>Защита:</b> <code>{protection}</code>
+        🏴‍☠️ <b>Взломана:</b>
+        <blockquote>{hacked}</blockquote>
+        """
+    ).strip()
 
 
 def get_items(_: NotificationJob) -> list[DataItem]:
     return [
         DataItem(
             value=game.url,
-            title=(
-                f"{game.title}\n(релиз {game.release_date:%d/%m/%Y}, "
-                f"взлом {game.crack_date:%d/%m/%Y}, защита {game.protection})"
-            ),
+            title=get_game_title(game),
             url=game.url,
+            need_html_escape_content=False,
         )
         for game in get_games()
     ]
