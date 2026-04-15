057341ae0cb75eb81c4a2236ecaa101812e8b900
gil9red <ilya.petrash@inbox.ru>
2026-04-15 23:07:44 +0500

    Added. Check new video dubaq

---
+++ Check new video dubaq/main.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "ipetrash"
+
+
+"""
+Скрипт для уведомления о появлении новых видео dubaq.
+
+"""
+
+
+import sys
+from pathlib import Path
+
+DIR = Path(__file__).resolve().parent
+sys.path.append(str(DIR.parent))  # Путь к папке выше
+
+from formats import FORMATS_VIDEO
+from root_common import (
+    run_notification_job,
+    SavedModeEnum,
+    get_yt_video_list,
+)
+
+run_notification_job(
+    "dubaq",
+    DIR,
+    lambda _: get_yt_video_list("https://www.youtube.com/@dubaq/videos"),
+    save_mode=SavedModeEnum.DATA_ITEM,
+    formats=FORMATS_VIDEO.replace(
+        prefix="🥶",
+    ),
+)
