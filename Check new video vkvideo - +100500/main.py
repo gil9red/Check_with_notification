e2ac55defc68995d28f50e18e693b8cf20ac8272
gil9red <ilya.petrash@inbox.ru>
2026-04-26 23:28:14 +0500

    Add: Check new video vkvideo - +100500

---
+++ Check new video vkvideo - +100500/main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "ipetrash"
+
+
+"""
+Скрипт для уведомления о появлении новых видео "+100500".
+
+"""
+
+
+import sys
+from pathlib import Path
+
+DIR = Path(__file__).resolve().parent
+sys.path.append(str(DIR.parent))  # Путь к папке выше
+
+from formats import FORMATS_VIDEO
+from root_common import (
+    run_notification_job,
+    SavedModeEnum,
+    get_vkvideo_video_list,
+)
+
+run_notification_job(
+    "+100500 [vkvideo]",
+    DIR,
+    lambda _: get_vkvideo_video_list("https://vkvideo.ru/playlist/-1719791_48513772"),
+    save_mode=SavedModeEnum.DATA_ITEM,
+    formats=FORMATS_VIDEO,
+)
