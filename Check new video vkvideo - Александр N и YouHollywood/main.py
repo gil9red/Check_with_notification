c08eaa2ec0bb1ac9545d57754f1b0d736be1028f
gil9red <ilya.petrash@inbox.ru>
2026-04-28 03:31:36 +0500

    Add: Check new video vkvideo - Александр N и YouHollywood

---
+++ Check new video vkvideo - Александр N и YouHollywood/main.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "ipetrash"
+
+
+"""
+Скрипт для уведомления о появлении новых видео Александр N и YouHollywood.
+
+"""
+
+
+import sys
+import re
+
+from pathlib import Path
+
+DIR = Path(__file__).resolve().parent
+sys.path.append(str(DIR.parent))  # Путь к папке выше
+
+from formats import FORMATS_VIDEO
+from root_common import (
+    DataItem,
+    NotificationJob,
+    SavedModeEnum,
+    get_vkvideo_video_list,
+    run_notification_job,
+)
+
+
+def get_items(_: NotificationJob) -> list[DataItem]:
+    # Не все видео интересны, убраны те, что связаны с курсами YouHollywood
+    url = "https://vkvideo.ru/@youhollywood/all"
+    return [
+        item
+        for item in get_vkvideo_video_list(url)
+        if (
+            # Слова в черном списке
+            not re.search(
+                "Проект Про|курс|школ",
+                item.title,
+                flags=re.IGNORECASE,
+            )
+            # NOTE: Перестал добавлять имя
+            # and "Александр N".lower() in item.title.lower()
+        )
+    ]
+
+
+run_notification_job(
+    "Александр N и YouHollywood [vkvideo]",
+    DIR,
+    get_items,
+    save_mode=SavedModeEnum.DATA_ITEM,
+    formats=FORMATS_VIDEO,
+)
