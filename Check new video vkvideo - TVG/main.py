8d72ad26b3a8d7c61f807df696feb8b4bf725117
gil9red <ilya.petrash@inbox.ru>
2026-04-28 23:48:37 +0500

    Add: Check new video vkvideo - TVG

---
+++ Check new video vkvideo - TVG/main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "ipetrash"
+
+
+"""
+Скрипт для уведомления о появлении новых видео TVG (Time Video Game).
+
+"""
+
+
+import sys
+from pathlib import Path
+
+DIR = Path(__file__).resolve().parent
+sys.path.append(str(DIR.parent))  # Путь к папке выше
+
+from formats import FORMATS_VIDEO
+from root_common import (
+    SavedModeEnum,
+    run_notification_job,
+    get_vkvideo_video_list,
+)
+
+run_notification_job(
+    "TVG [vkvideo]",
+    DIR,
+    lambda _: get_vkvideo_video_list("https://vkvideo.ru/@timevideogame/all"),
+    save_mode=SavedModeEnum.DATA_ITEM,
+    formats=FORMATS_VIDEO,
+)
