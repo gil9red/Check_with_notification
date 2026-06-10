fed5f5e06fc666d1d19bf16a26bf0e7ffbc9747f
gil9red <ilya.petrash@inbox.ru>
2026-06-10 22:28:58 +0500

    feat: add "Check new video vkvideo - StopGame"

---
+++ Check new video vkvideo - StopGame/main.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "ipetrash"
+
+
+"""
+Скрипт для уведомления о появлении новых видео Разбора полётов и Истории серий на канале StopGame.
+
+"""
+
+
+import sys
+from pathlib import Path
+
+DIR = Path(__file__).resolve().parent
+sys.path.append(str(DIR.parent))  # Путь к папке выше
+
+from formats import FORMATS_VIDEO
+from root_common import (
+    DataItem,
+    NotificationJob,
+    SavedModeEnum,
+    run_notification_job,
+    get_vkvideo_video_list,
+)
+
+
+def get_items(_: NotificationJob) -> list[DataItem]:
+    url = "https://vkvideo.ru/@stopgameru/all"
+    return [
+        item
+        for item in get_vkvideo_video_list(url)
+        if "Разбор полётов" in item.title or "История серии" in item.title
+    ]
+
+
+run_notification_job(
+    "StopGame [vkvideo]",
+    DIR,
+    get_items,
+    save_mode=SavedModeEnum.DATA_ITEM,
+    formats=FORMATS_VIDEO,
+)
